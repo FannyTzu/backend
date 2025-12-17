40300d5a9c5973ade4857b9db28c77161f067fb0
FannyTzu <fannytzu@gmail.com>
2025-12-17 11:13:19 +0100

    add applyChoice endpoint to update player state and modify player routes

--- controllers/players.controller.js
+++ controllers/players.controller.js
@@ -1,4 +1,5 @@
 import { players } from "../data/players.js";
+import { pages } from "../data/pages.js";
 
 export const getAllPlayers
   = (req, res) => {
@@ -37,4 +38,35 @@ export const deletePlayer = (req, res) => {
 
   const deleted = players.splice(index, 1);
   res.json(deleted[0]);
+};
+
+//endpoint to apply a choice and update player state
+
+export const applyChoice = (req, res) => {
+  const playerId = Number(req.params.id);
+  const { nextPageId } = req.body;
+
+  const player = players.find(p => p.id === playerId);
+  const page = pages.find(p => p.id === nextPageId);
+
+  if (!player || !page) {
+    return res.status(404).json({ message: "Player or page not found" });
+  }
+
+  // impacts
+  page.impact?.forEach(effect => {
+    if (effect.endurance) player.endurance += effect.endurance;
+    if (effect.money) player.money += effect.money;
+  });
+
+  // items
+  page.items?.forEach(item => {
+    if (item.weapons) player.weapons.push(item.weapons);
+    if (item.money) player.money += item.money;
+    if (item.stuff) player.stuff.push(...item.stuff);
+  });
+
+  player.currentPageId = nextPageId;
+
+  res.json({ player, page });
 };
