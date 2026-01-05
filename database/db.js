eddd4c10a7dba82467b28f8f2392b7e3346204b6
FannyTzu <fannytzu@gmail.com>
2026-01-05 14:04:46 +0100

    feat(database): streamline database connection configuration

--- database/db.js
+++ database/db.js
@@ -2,13 +2,11 @@ import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const { Pool } = pkg;
 
+const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // SSL obligatoire pour Neon
 });
